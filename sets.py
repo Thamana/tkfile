8fac8fdc80a65ebecc4c129d2e9193af255eb932
Thamana <77269885+Thamana@users.noreply.github.com>
2023-09-20 20:30:22 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a={1,2,3,4,5,6,7,8}
+b={2,4,6,8,10,12,16}
+a.union(b) #union
+
+
+# In[2]:
+
+
+a.intersection(b) #intersection
+
+
+# In[3]:
+
+
+a.difference(b) #a-b
+
+
+# In[4]:
+
+
+b.difference(a) #b-a
+
+
+# In[ ]:
+
+
+
+
